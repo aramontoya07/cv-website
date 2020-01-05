185f87e9bc9a0539722e0fb90f1a047306abeb45
aramontoya07 <aramontoya07@gmail.com>
2020-01-05 16:35:32 -0300

    elimino archivos sin uso y agrego el js file

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let nav = document.getElementById('nav');
+let menu = document.getElementById('enlaces');
+
+window.addEventListener('scroll', function(){
+    this.console.log(window.pageYOffset);
+});
