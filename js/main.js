307c9cf857b87a5b8c14f7ecfc11d0a9f5c458de
aramontoya07 <aramontoya07@gmail.com>
2020-03-14 16:59:40 -0300

    termino el menu responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let nav = document.getElementById('nav');
 let menu = document.getElementById('enlaces');
+let abrir = document.getElementById('open');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true;
 
 /* Funcion que permite que el nav se haga grande y no transparente */
 function menus(){
@@ -8,21 +11,58 @@ function menus(){
         nav.classList.remove('nav2');
         nav.className = ('nav1');
         nav.style.transition = '1s';
+        abrir.style.color = '#fff';
+        
     } else{
         nav.classList.remove('nav1');
         nav.className = ('nav2');
         nav.style.transition = '1s';
+        nav.style.color = '#000';
     }
 }
 
-window.addEventListener('scroll', function(){
-    this.console.log(window.pageYOffset);
-    menus();
-});
-
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = hidden;
+        cerrado = true;
+    }
+}
 window.addEventListener('load', function(){
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
     menus();
 
 });
+
+window.addEventListener('click', function(e){ //para que se cierre al hacer click en cualquier otro lado de la pantalla que no sea el hamburguesa
+    if(cerrado == false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            menu.style.width = '0%';
+            menu.style.overflow = "hidden";
+            cerrado = true;
+        }
+    }
+});
+
+window.addEventListener('scroll', function(){
+    this.console.log(window.pageYOffset);
+    menus();
+});
+
+window.addEventListener('resize', function(){
+    if(screen.width >= 700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+});
+
+abrir.addEventListener('click', function(){
+    apertura();
+});
+
